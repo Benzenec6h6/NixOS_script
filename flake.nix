a1e3cecede95b1f936d57dac3d239ee4c563cc1d
Benzenec6h6 <aconitinec34h47no11@gmail.com>
2025-08-21 18:17:00 +0900

    add unfree

--- flake.nix
+++ flake.nix
@@ -1,32 +1,39 @@
 {
-  description = "NixOS 25.05 + Home Manager 25.05";
+  description = "My NixOS config with Home Manager (NixOS 25.05)";
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
-    home-manager = {
-      url = "github:nix-community/home-manager/release-25.05";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
+    home-manager.url = "github:nix-community/home-manager/release-25.05";
+    home-manager.inputs.nixpkgs.follows = "nixpkgs";
   };
 
   outputs = { self, nixpkgs, home-manager, ... }:
     let
       system = "x86_64-linux";
-      pkgs = import nixpkgs { inherit system; };
+
+      # nixpkgs のインスタンスを作成する時に unfree の設定を入れる
+      pkgs = import nixpkgs {
+        inherit system;
+        config = {
+          allowUnfree = true;
+
+          # 必要な unfree パッケージだけを許可
+          allowUnfreePredicate = pkg: builtins.elem (nixpkgs.lib.getName pkg) [
+            "nvidia-x11"
+            "nvidia-settings"
+            "cudatoolkit"
+            "cuda-merged"
+          ];
+        };
+      };
     in {
       nixosConfigurations.my-nixos = nixpkgs.lib.nixosSystem {
-        inherit system pkgs;
+        inherit system;
+        specialArgs = { inherit pkgs; };
         modules = [
           ./configuration.nix
           home-manager.nixosModules.home-manager
         ];
-      };
-
-      homeConfigurations.teto = home-manager.lib.homeManagerConfiguration {
-        inherit pkgs;
-        username = "teto";
-        homeDirectory = "/home/teto";
-        configuration = ./home/teto.nix;
       };
     };
 }
