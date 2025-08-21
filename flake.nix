1ca6c5c120b9d685e7e88f62d7dee6482331ea11
Benzenec6h6 <aconitinec34h47no11@gmail.com>
2025-08-21 14:30:00 +0900

    version 25.05

--- flake.nix
+++ flake.nix
@@ -1,10 +1,10 @@
 {
-  description = "NixOS + Home Manager standalone configuration";
+  description = "NixOS 25.05 + Home Manager 25.05";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
     home-manager = {
-      url = "github:nix-community/home-manager";
+      url = "github:nix-community/home-manager/release-25.05";
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
