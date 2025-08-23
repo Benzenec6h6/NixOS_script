12a039b63126cfb05cdbb6bc8efb829f4ab96470
Benzenec6h6 <aconitinec34h47no11@gmail.com>
2025-08-23 18:32:39 +0900

    mesa

---
+++ modules/desktop-vm.nix
@@ -0,0 +1,91 @@
+{ config, pkgs, lib, ... }:
+
+{
+  # X11 / Wayland
+  services.xserver.enable = true;
+
+  # ログインマネージャ（LightDM）を有効化
+  services.xserver.displayManager.lightdm.enable = true;
+
+  # KDE Plasma 6 を有効化
+  #services.xserver.desktopManager.plasma6.enable = true;
+
+  # Wayland / wlroots / Common Applications
+  environment.systemPackages = with pkgs; [
+    # --- Xorg 必須 ---
+    xorg.xorgserver xorg.xmessage xorg.xrandr xorg.xev
+
+    # --- Wayland 関連 ---
+    wayland wayland-protocols libxkbcommon
+
+    # --- wlroots/i3 用 ---
+    wlr-randr
+
+    # --- XDG portals (DE/WM 統合用) ---
+    xdg-desktop-portal kdePackages.xdg-desktop-portal-kde xdg-desktop-portal-wlr
+
+    # Virtualization
+    qemu_full libvirt OVMF dnsmasq swtpm libosinfo virt-viewer
+
+    # Containers
+    docker
+
+    # Audio / video
+    pipewire wireplumber
+  ];
+
+  # GPU / Microcode
+  hardware.opengl.enable = true;
+
+  # VM gpu driver
+  services.xserver.videoDrivers = [ "modesetting" ];
+  hardware.nvidia.enable = false;
+  hardware.opengl.extraPackages = with pkgs; [
+    mesa.drivers
+  ];
+  
+  # CPU microcode
+  boot.kernelPackages = pkgs.linuxPackages;
+  hardware.cpu.intel.updateMicrocode = true;
+  hardware.cpu.amd.updateMicrocode = false;
+
+  # Power management & services
+  services.power-profiles-daemon.enable = false;
+  services.tlp.enable = true;
+  services.printing.enable = true;
+  services.blueman.enable = true;
+
+  # Input Method / Fonts
+  i18n.inputMethod.enabled = "fcitx5";
+  i18n.inputMethod.fcitx5.addons = with pkgs; [ fcitx5-mozc ];
+
+  fonts = {
+    enableDefaultFonts = true;
+    packages = with pkgs; [
+      noto-fonts
+      noto-fonts-cjk-sans
+      noto-fonts-cjk-serif
+      noto-fonts-emoji
+      jetbrains-mono
+      fira-code
+      hack-font
+    ];
+    fontconfig = {
+      enable = true;
+      defaultFonts = {
+        sansSerif = [ "Noto Sans CJK JP" ];
+        serif     = [ "Noto Serif CJK JP" ];
+        monospace = [ "JetBrains Mono" "Fira Code" "Hack" "Cascadia Code" ];
+      };
+    };
+  };
+
+  # Docker
+  virtualisation.docker.enable = true;
+
+  # Extra startup
+  services.xserver.displayManager.sessionCommands = ''
+    nm-applet &
+    fcitx5 -d &
+  '';
+}
