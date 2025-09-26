4a39f340a0512a86c0ff56aea520db2609028c5d
Benzenec6h6 <aconitinec34h47no11@gmail.com>
2025-09-26 17:29:25 +0900

    delete \

--- install.sh
+++ install.sh
@@ -107,8 +107,7 @@ nixos-generate-config --root /mnt
 
 # --- NixOS インストール ---
 echo "=== NixOS をインストールします ==="
-cp -r /home/nixos/NixOS_script \
-   /mnt/etc/nixos/
+cp -r /home/nixos/NixOS_script /mnt/etc/nixos/
 
 cp /mnt/etc/nixos/hardware-configuration.nix \
    /mnt/etc/nixos/NixOS_script/system/hosts/hardware.nix
