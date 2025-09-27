203ac17a9128c77d3b19129146a3147918a42578
Benzenec6h6 <aconitinec34h47no11@gmail.com>
2025-09-27 16:37:25 +0900

    kernelmodules

--- system/hosts/vm.nix
+++ system/hosts/vm.nix
@@ -16,6 +16,7 @@
   # VM特有の設定
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
+  boot.initrd.kernelModules = [ "virtio_pci" "virtio_blk" ];
 
   # QEMU の guest agent を使う場合など
   services.qemuGuest.enable = true;
