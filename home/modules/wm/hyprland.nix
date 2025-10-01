b53e19f80b03247482c7b2c1cbcd13c70e75ba6a
Benzenec6h6 <aconitinec34h47no11@gmail.com>
2025-10-01 14:55:52 +0900

    frist commit

---
+++ home/modules/wm/hyprland.nix
@@ -0,0 +1,80 @@
+{ config, pkgs, ... }:
+
+{
+  wayland.windowManager.hyprland = {
+    enable = true;
+    settings = {
+      # モニタ設定
+      monitor = [
+        "eDP-1,1920x1080,0x0,1"
+      ];
+
+      # 起動時に実行するコマンド
+      exec-once = [
+        "waybar"
+        "fcitx5"
+      ];
+
+      # キーバインド例
+      bind = [
+        "SUPER,Return,exec,kitty"
+        "SUPER,Q,killactive,"
+        "SUPER,E,exec,thunar"
+        "SUPER,F,fullscreen,"
+        "SUPER,Space,exec,rofi --show drun"
+      ];
+
+      general = {
+        gaps_in = 5;
+        gaps_out = 20;
+        border_size = 2;
+      };
+
+      decoration = {
+        rounding = 10;
+        blur = {
+          enabled = true;
+          size = 8;
+          passes = 2;
+        };
+      };
+    };
+  };
+
+  # 便利ツール
+  home.packages = with pkgs; [
+    waybar ags eww
+    kitty
+    celluloid
+    blueman
+    swww mpvpaper
+    pavucontrol #playerctl
+    grim slurp wf-recorder wl-clipboard
+    swappy
+    #rofi-wayland #rofi
+    #xdg-desktop-portal-hyprland
+    #cava
+    #xfce.thunar
+    #swaynotificationcenter #swaync
+  ];
+
+  programs.waybar = {
+    enable = true;
+    package = pkgs.waybar;
+    # ここでは config/style を特に書かずデフォルトに任せる
+  };
+
+  programs.rofi = {
+    enable = true;
+    package = pkgs.rofi-wayland; 
+    theme = "Arc-Dark";          # デフォルトテーマを指定
+    extraConfig = {
+      modi = "drun,run,window";
+      show-icons = true;
+    };
+  };
+
+  programs.playerctl.enable = true;
+  programs.cava.enable = true;
+  services.swaync.enable = true;
+}
