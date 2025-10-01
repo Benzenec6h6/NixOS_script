2ebb76325c4b8ec1424fbc434d403f2b6d3e15f4
Benzenec6h6 <aconitinec34h47no11@gmail.com>
2025-10-01 14:52:39 +0900

    delete login shell zsh

--- system/modules/users.nix
+++ system/modules/users.nix
@@ -10,7 +10,6 @@
       description = "Main user";
       extraGroups = [ "networkmanager" "wheel" "libvirtd" "scanner" "lp" "video" "input"  "audio" "docker" "libvirtd" "kvm" ];
       hashedPassword = "$6$qPo6ahBPqNC7mMim$WupFSLamdZfSEoafxSoE1ODgtaHS8gmUayQ2dTiW4vDBAVVJDcuj1yMYAHq.tz5mmZW7aqb44KnMacSq12xpO1";
-      shell = pkgs.zsh;
     };
   };
 
