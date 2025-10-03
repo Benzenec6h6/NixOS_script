657fd1827b3abd08afced4d7ac603cda9655a6d9
Benzenec6h6 <aconitinec34h47no11@gmail.com>
2025-10-03 15:45:53 +0900

    add shortcuts

--- home/modules/wm/hyprland.nix
+++ home/modules/wm/hyprland.nix
@@ -19,10 +19,57 @@
       # キーバインド例
       bind = [
         "SUPER,Return,exec,kitty"
+        "SUPER,B,exec,zen"  
         "SUPER,Q,killactive,"
         "SUPER,E,exec,thunar"
         "SUPER,F,fullscreen,"
+        "SUPER,H,exec,~/.config/hypr/help_shortcuts.sh"
         "SUPER,Space,exec,rofi --show drun"
+
+        ",XF86AudioRaiseVolume,exec,wpctl set-volume @DEFAULT_AUDIO_SINK@ 5%+"
+        ",XF86AudioLowerVolume,exec,wpctl set-volume @DEFAULT_AUDIO_SINK@ 5%-"
+        ",XF86AudioRaiseVolume,SHIFT,exec,wpctl set-volume @DEFAULT_AUDIO_SINK@ 1%+"
+        ",XF86AudioLowerVolume,SHIFT,exec,wpctl set-volume @DEFAULT_AUDIO_SINK@ 1%-"
+
+        ",XF86AudioMute,exec,wpctl set-mute @DEFAULT_AUDIO_SINK@ toggle"
+        ",XF86AudioPlay,exec,playerctl play-pause"
+        ",XF86AudioPause,exec,playerctl play-pause"
+        ",XF86AudioNext,exec,playerctl next"
+        ",XF86AudioPrev,exec,playerctl previous"
+
+        ",XF86MonBrightnessDown,exec,brightnessctl set 5%-"
+        ",XF86MonBrightnessUp,exec,brightnessctl set +5%"
+        ",XF86MonBrightnessDown,SHIFT,exec,brightnessctl set 1%-"
+        ",XF86MonBrightnessUp,SHIFT,exec,brightnessctl set +1%"
+
+        "$modifier,1,workspace,1"
+        "$modifier,2,workspace,2"
+        "$modifier,3,workspace,3"
+        "$modifier,4,workspace,4"
+        "$modifier,5,workspace,5"
+        "$modifier,6,workspace,6"
+        "$modifier,7,workspace,7"
+        "$modifier,8,workspace,8"
+        "$modifier,9,workspace,9"
+        "$modifier,0,workspace,10"
+
+        "$modifier SHIFT,1,movetoworkspace,1"
+        "$modifier SHIFT,2,movetoworkspace,2"
+        "$modifier SHIFT,3,movetoworkspace,3"
+        "$modifier SHIFT,4,movetoworkspace,4"
+        "$modifier SHIFT,5,movetoworkspace,5"
+        "$modifier SHIFT,6,movetoworkspace,6"
+        "$modifier SHIFT,7,movetoworkspace,7"
+        "$modifier SHIFT,8,movetoworkspace,8"
+        "$modifier SHIFT,9,movetoworkspace,9"
+        "$modifier SHIFT,0,movetoworkspace,10"
+
+        ",CONTROL, ALT,right,workspace,e+1"
+        ",CONTROL, ALT,left,workspace,e-1"
+        "$modifier,mouse_down,workspace,e+1"
+        "$modifier,mouse_up,workspace,e-1"
+        "$modifier SHIFT,SPACE,movetoworkspace,special"
+        "$modifier,SPACE,togglespecialworkspace"
       ];
 
       # キーボードレイアウトの設定
