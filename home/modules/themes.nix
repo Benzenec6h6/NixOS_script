2ed97d4c38e87c01ccb7ae2005eff3b30496f871
Benzenec6h6 <aconitinec34h47no11@gmail.com>
2025-10-08 18:46:16 +0900

    delete fonts

--- home/modules/themes.nix
+++ home/modules/themes.nix
@@ -39,44 +39,7 @@
     gruppled-black-cursors
     catppuccin-cursors
     capitaine-cursors-themed
-
-    # フォント
-    noto-fonts
-    noto-fonts-cjk-sans
-    noto-fonts-cjk-serif
-    noto-fonts-color-emoji
-    font-awesome
   ];
-
-  # GTK テーマ設定 (Catppuccin をデフォルトに)
-  gtk = {
-    enable = true;
-    theme = {
-      name = "Catppuccin-Mocha-Standard-Blue-Dark";
-      package = pkgs.catppuccin-gtk;
-    };
-    iconTheme = {
-      name = "Papirus";
-      package = pkgs.papirus-icon-theme;
-    };
-    cursorTheme = {
-      name = "Catppuccin-Mocha-Dark";
-      package = pkgs.catppuccin-cursors;
-    };
-  };
-
-  # Qt 側の設定 (Kvantum + GTK fallback)
-  qt = {
-    enable = true;
-    style.name = "kvantum";
-    platformTheme.name = "gtk"; # GTK と統一
-  };
-
-  # Kvantum 設定を XDG に書き込み（Catppuccinをデフォルトに）
-  xdg.configFile."Kvantum/kvantum.kvconfig".text = ''
-    [General]
-    theme=Catppuccin-Mocha-Blue
-  '';
 
   fonts.fontconfig.enable = true;
 }
