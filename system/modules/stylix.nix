667c3d961b93a3eb314548032f7f9d2ed4f303b7
Benzenec6h6 <aconitinec34h47no11@gmail.com>
2025-10-09 18:32:10 +0900

    retry

---
+++ system/modules/stylix.nix
@@ -0,0 +1,44 @@
+{ config, pkgs, lib, ... }:
+{
+  stylix = {
+    enable = true;
+
+    # 壁紙など Stylix に任せたくないなら image は指定しない
+    image = null; #← 書かなくてもOK
+
+    # テーマの基本方向（dark / light）
+    polarity = "dark";
+
+    #テーマ設定
+    base16Scheme = "${pkgs.base16-schemes}/share/themes/tokyo-night-dark.yaml";
+
+    # カーソルテーマ
+    cursor = {
+      package = pkgs.bibata-cursors;
+      name = "Bibata-Modern-Ice";
+      size = 24;
+    };
+
+    # フォント設定
+    fonts = {
+      monospace = {
+        package = pkgs.nerd-fonts.jetbrains-mono;
+        name = "JetBrains Mono";
+      };
+      sansSerif = {
+        package = pkgs.montserrat;
+        name = "Montserrat";
+      };
+      serif = {
+        package = pkgs.montserrat;
+        name = "Montserrat";
+      };
+      sizes = {
+        applications = 12;
+        terminal = 15;
+        desktop = 11;
+        popups = 12;
+      };
+    };
+  };
+}
