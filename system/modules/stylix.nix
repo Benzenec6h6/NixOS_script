ad398d72044b215c0f1fa9591d41545a5116b815
Benzenec6h6 <aconitinec34h47no11@gmail.com>
2025-10-10 15:43:26 +0900

    add color palette

--- system/modules/stylix.nix
+++ system/modules/stylix.nix
@@ -11,10 +11,26 @@
 
     #テーマ設定
     base16Scheme = "${pkgs.base16-schemes}/share/themes/tokyo-night-dark.yaml";
-    #base16Scheme = stylix.lib.base16.schemeFromYAML {
-    #  path = "${pkgs.base16-schemes}/share/themes/tokyo-night-dark.yaml";
-    #};
-
+    /*
+    base16Scheme = {
+      base00 = "282936";
+      base01 = "3a3c4e";
+      base02 = "4d4f68";
+      base03 = "626483";
+      base04 = "62d6e8";
+      base05 = "e9e9f4";
+      base06 = "f1f2f8";
+      base07 = "f7f7fb";
+      base08 = "ea51b2";
+      base09 = "b45bcf";
+      base0A = "00f769";
+      base0B = "ebff87";
+      base0C = "a1efe4";
+      base0D = "62d6e8";
+      base0E = "b45bcf";
+      base0F = "00f769";
+    };
+    */
     # カーソルテーマ
     cursor = {
       package = pkgs.bibata-cursors;
