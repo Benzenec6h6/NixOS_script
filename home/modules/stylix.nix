6191cafbd7b2cb1b14b397d7bb3bfac62e043458
Benzenec6h6 <aconitinec34h47no11@gmail.com>
2025-10-11 16:45:40 +0900

    import stylix

---
+++ home/modules/stylix.nix
@@ -0,0 +1,13 @@
+_: {
+  stylix.targets = {
+    gtk.enable = true;
+    waybar.enable = false;
+    rofi.enable = false;
+    hyprland.enable = false;
+    hyprlock.enable = false;
+    qt = {
+      enable = true;
+      platform = "qtct";
+    };
+  };
+}
