4e4f5bee27fe4215b5495589dfbedb7165eb2169
Benzenec6h6 <aconitinec34h47no11@gmail.com>
2025-10-17 13:35:28 +0900

    change color config

--- modules/home/rofi.nix
+++ modules/home/rofi.nix
@@ -8,15 +8,21 @@
       icon-theme = "Papirus";
       font = "JetBrainsMono Nerd Font Mono 12";
     };
+
     theme = let inherit (config.lib.formats.rasi) mkLiteral; in {
       "*" = {
-        bg = mkLiteral "#${config.stylix.base16Scheme.base00}";
-        fg = mkLiteral "#${config.stylix.base16Scheme.base05}";
-        selected = mkLiteral "#${config.stylix.base16Scheme.base08}";
+        background = mkLiteral "#${config.lib.stylix.colors.base00}";
+        foreground = mkLiteral "#${config.lib.stylix.colors.base05}";
       };
+
       "window" = {
         border-radius = mkLiteral "15px";
-        transparency = "real";
+        background-color = mkLiteral "#${config.lib.stylix.colors.base00}";
+      };
+
+      "element selected" = {
+        background-color = mkLiteral "#${config.lib.stylix.colors.base08}";
+        text-color = mkLiteral "#${config.lib.stylix.colors.base00}";
       };
     };
   };
