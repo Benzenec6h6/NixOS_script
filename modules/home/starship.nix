ea8bbac0e80fb71d9ccc0a9a93799c6fed529c92
Benzenec6h6 <aconitinec34h47no11@gmail.com>
2025-10-17 17:50:25 +0900

    for complement and exterior

---
+++ modules/home/starship.nix
@@ -0,0 +1,40 @@
+{ config, pkgs, ... }:
+{
+  programs.starship = {
+    enable = true;
+    enableZshIntegration = true;
+    settings = {
+      add_newline = false;
+      format = "$all$character";
+
+      [character]
+      success_symbol = "[❯](bold green)";
+      error_symbol = "[❯](bold red)";
+
+      [git_branch]
+      symbol = "🌱 ";
+      truncation_length = 24;
+
+      [nix_shell]
+      symbol = " ";
+      format = "[$symbol$state( $name)]($style) ";
+
+      [directory]
+      truncation_length = 3;
+      style = "bold blue";
+
+      [time]
+      disabled = false;
+      format = "[$time]($style) ";
+      time_format = "%H:%M";
+
+      [cmd_duration]
+      disabled = false;
+      format = "took [$duration]($style) ";
+
+      [python]
+      symbol = " ";
+      format = "[$symbol$version]($style) ";
+    };
+  };
+}
