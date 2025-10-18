ef19a6c601447a25d1755d38ba463ecb156620fb
Benzenec6h6 <aconitinec34h47no11@gmail.com>
2025-10-18 17:07:32 +0900

    delete quickshell

--- flake.nix
+++ flake.nix
@@ -14,6 +14,11 @@
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
+    #quickshell = {
+    #  url = "git+https://git.outfoxxed.me/outfoxxed/quickshell";
+    #  inputs.nixpkgs.follows = "nixpkgs";
+    #};
+
     zen-browser.url = "github:MarceColl/zen-browser-flake";
   };
 
