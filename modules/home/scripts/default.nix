f1f921da9d6f071f881462d92d5e8e32a3f55585
Benzenec6h6 <aconitinec34h47no11@gmail.com>
2025-11-01 18:44:13 +0900

    import overview

--- modules/home/scripts/default.nix
+++ modules/home/scripts/default.nix
@@ -6,6 +6,7 @@
   home.packages = [
     (import ./Brightness.nix { inherit pkgs; })
     (import ./KeyBinds.nix { inherit pkgs; })
+    (import ./overview.nix { inherit pkgs; })
     (import ./toggle-waybar.nix { inherit pkgs; })
     (import ./Volume.nix { inherit pkgs; })
     (import ./WaybarCava.nix { inherit pkgs; })
