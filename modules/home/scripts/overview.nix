b4a5b49c2a9f74b86f5a78065b48fdac0f309378
Benzenec6h6 <aconitinec34h47no11@gmail.com>
2025-11-02 16:43:43 +0900

    change config to home

--- modules/home/scripts/overview.nix
+++ modules/home/scripts/overview.nix
@@ -1,4 +1,4 @@
 { pkgs }:
 pkgs.writeShellScriptBin "overview" ''
-  pgrep -f overview.qml >/dev/null && pkill -f overview.qml || quickshell ${config.home.homeDirectory}/.config/quickshell/overview.qml
+  pgrep -f overview.qml >/dev/null && pkill -f overview.qml || quickshell ''$HOME/.config/quickshell/overview.qml
 ''
