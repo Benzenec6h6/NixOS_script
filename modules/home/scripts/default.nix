d24087a448992edea9aafac4db8f7ba5897c98c7
Benzenec6h6 <aconitinec34h47no11@gmail.com>
2025-11-05 03:39:09 +0900

    add inputs

--- modules/home/scripts/default.nix
+++ modules/home/scripts/default.nix
@@ -1,12 +1,9 @@
-{ pkgs
-, username
-, profile
-, ...
-}: {
+{ pkgs, username, profile, inputs, ...}:
+{
   home.packages = [
     (import ./Brightness.nix { inherit pkgs; })
     (import ./KeyBinds.nix { inherit pkgs; })
-    (import ./overview.nix { inherit pkgs; })
+    (import ./overview.nix { inherit pkgs inputs; })
     (import ./toggle-waybar.nix { inherit pkgs; })
     (import ./Volume.nix { inherit pkgs; })
     (import ./WaybarCava.nix { inherit pkgs; })
