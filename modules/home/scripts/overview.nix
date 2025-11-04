0c2285140ce2f8a8928b5652d7d376da91706d21
Benzenec6h6 <aconitinec34h47no11@gmail.com>
2025-11-05 03:26:37 +0900

    change pkg scope

--- modules/home/scripts/overview.nix
+++ modules/home/scripts/overview.nix
@@ -1,9 +1,9 @@
-{ pkgs }:
+{ pkgs, inputs, ... }:
 pkgs.writeShellScriptBin "overview" ''
   export HYPRLAND_INSTANCE_SIGNATURE=$(cat /tmp/hypr/$(whoami)*/.instance)
   if pgrep -f quickshell.*overview.qml >/dev/null; then
     pkill -f quickshell.*overview.qml
   else
-    ${pkgs.quickshell}/bin/quickshell -p $HOME/.config/quickshell/overview.qml &
+    ${inputs.quickshell.packages.${pkgs.system}.default}/bin/quickshell -p $HOME/.config/quickshell/overview.qml &
   fi
 ''
