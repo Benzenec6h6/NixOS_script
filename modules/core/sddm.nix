541a2c0a69ab23bc25be12b0ff118d42258d08f6
Benzenec6h6 <aconitinec34h47no11@gmail.com>
2025-11-11 04:28:47 +0900

    fixed extrapackages

--- modules/core/sddm.nix
+++ modules/core/sddm.nix
@@ -1,14 +1,24 @@
 { config, pkgs, ... }:
 
 {
-  services.displayManager ={
+  services.displayManager = {
     defaultSession = "hyprland";
     sddm ={
       enable = true;
       wayland.enable = true;
-      package = pkgs.kdePackages.sddm;
-      extraPackages = [pkgs.sddm-astronaut];
-      theme = "sddm-astronaut-theme";
+      package = pkgs.kdePackages.sddm;   # ← これは「SDDM本体」
+
+      theme = "sddm-astronaut-theme";    # ← テーマ名
+      extraPackages = with pkgs.kdePackages; [
+        qtmultimedia
+        qtvirtualkeyboard
+        qtsvg
+      ];
     };
   };
+
+  environment.systemPackages = with pkgs; [
+    kdePackages.qtbase 
+    sddm-astronaut                # ← これは「テーマ」
+  ];
 }
