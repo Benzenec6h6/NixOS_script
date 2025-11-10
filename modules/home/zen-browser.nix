11e04db16d1826481689f8bd4f0179270e3da171
Benzenec6h6 <aconitinec34h47no11@gmail.com>
2025-11-11 04:39:35 +0900

    zen-browser config

---
+++ modules/home/zen-browser.nix
@@ -0,0 +1,50 @@
+{ config, pkgs, inputs, ... }:
+
+{
+  # Firefox モジュールとして Zen Browser を設定
+  programs.firefox = {
+    enable = true;
+    package = inputs.zen-browser.packages.${pkgs.system}.default;
+
+    profiles.default = {
+      id = 0;
+      name = "default";
+      isDefault = true;
+
+      # Firefox の設定 (about:config 相当)
+      settings = {
+        "browser.startup.homepage" = "https://example.org";
+        "browser.tabs.drawInTitlebar" = true;
+        "widget.use-xdg-desktop-portal.file-picker" = 1;
+        "widget.wayland-dmabuf-vaapi.enabled" = true;
+        "gfx.webrender.all" = true;
+        "media.ffmpeg.vaapi.enabled" = true;
+      };
+
+      # 推奨拡張機能（NUR 経由で取得）
+      packages = with pkgs.nur.repos.rycee.firefox-addons; [
+        ublock-origin
+        bitwarden
+        darkreader
+        tridactyl
+        to-deepl
+        multi-account-containers
+      ];
+
+      # 検索エンジン設定
+      search = {
+        default = "DuckDuckGo";
+        engines = {
+          "DuckDuckGo" = {
+            urls = [{ template = "https://duckduckgo.com/?q={searchTerms}"; }];
+            icon = "https://duckduckgo.com/favicon.ico";
+          };
+          "Google" = {
+            urls = [{ template = "https://www.google.com/search?q={searchTerms}"; }];
+            icon = "https://www.google.com/favicon.ico";
+          };
+        };
+      };
+    };
+  };
+}
