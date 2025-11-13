2a5e9e032d2ec8dcd58e850e57f735d53d2a6124
Benzenec6h6 <aconitinec34h47no11@gmail.com>
2025-11-13 17:44:59 +0900

    add starship config

---
+++ modules/home/starship/default.nix
@@ -0,0 +1,20 @@
+{ config, lib, ...}:
+
+{
+    programs.starship = {
+        enable = true;
+
+        # 外部 TOML ファイルを読み込む
+        settings = builtins.fromTOML (builtins.readFile ./starship.toml);
+
+        # 好みでシェル統合を設定
+        enableZshIntegration = true;
+        enableBashIntegration = true;
+        enableFishIntegration = false;
+
+        # ログイン時に Starship を確実にロードしたい場合
+        # initExtraCommand = ''
+        #   eval "$(starship init zsh)"
+        # '';
+    };
+}
