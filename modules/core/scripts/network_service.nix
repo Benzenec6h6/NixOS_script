c5012d93faf1fbd51a489a87663578c0be796c4b
Benzenec6h6 <aconitinec34h47no11@gmail.com>
2025-11-27 22:59:12 +0900

    fixed attrset to list

--- modules/core/scripts/network_service.nix
+++ modules/core/scripts/network_service.nix
@@ -1,24 +1,25 @@
 { pkgs, ... }:
 
 {
-  networking.networkmanager.enable = true;
+  networking.networkmanager.dispatcherScripts = [
+    {
+      name = "networkNotify";
+      source = pkgs.writeShellScript "nm-notify" ''
+        #!/usr/bin/env bash
 
-  networking.networkmanager.dispatcherScripts.networkNotify = {
-    source = pkgs.writeShellScript "nm-notify" ''
-      #!/usr/bin/env bash
+        IFACE="$1"
+        ACTION="$2"
 
-      IFACE="$1"
-      ACTION="$2"
-
-      case "$ACTION" in
-        up)
-          SSID=$(nmcli -t -f NAME connection show --active | head -n1)
-          network-bluetooth-notify network connected "$SSID"
-          ;;
-        down)
-          network-bluetooth-notify network disconnected
-          ;;
-      esac
-    '';
-  };
+        case "$ACTION" in
+          up)
+            SSID=$(nmcli -t -f NAME connection show --active | head -n1)
+            network-bluetooth-notify network connected "$SSID"
+            ;;
+          down)
+            network-bluetooth-notify network disconnected
+            ;;
+        esac
+      '';
+    }
+  ];
 }
