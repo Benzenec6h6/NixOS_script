45e6de8df6ae29998f8ec93d786d44a3a772e23f
Benzenec6h6 <aconitinec34h47no11@gmail.com>
2025-11-28 16:06:52 +0900

    change pw-play to pipewire

--- modules/home/scripts/ScreenShot.nix
+++ modules/home/scripts/ScreenShot.nix
@@ -2,7 +2,7 @@
 
 pkgs.writeShellScriptBin "screenshot" ''
   #!${pkgs.bash}/bin/bash
-  export PATH=${pkgs.hyprland}/bin:${pkgs.grim}/bin:${pkgs.slurp}/bin:${pkgs.wl-clipboard}/bin:${pkgs.jq}/bin:${pkgs.swappy}/bin:${pkgs.pw-play}/bin:$PATH
+  export PATH=${pkgs.hyprland}/bin:${pkgs.grim}/bin:${pkgs.slurp}/bin:${pkgs.wl-clipboard}/bin:${pkgs.jq}/bin:${pkgs.swappy}/bin:${pkgs.pipewire}/bin:$PATH
 
   ${builtins.readFile ./ScreenShots.sh}
 ''
