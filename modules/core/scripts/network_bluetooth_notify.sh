344534319014eedc6cac78387f65b2c281eea4d6
Benzenec6h6 <aconitinec34h47no11@gmail.com>
2025-11-28 16:46:02 +0900

    split settings network_bluetooth_notify.nix

---
+++ modules/core/scripts/network_bluetooth_notify.sh
@@ -0,0 +1,42 @@
+#!/usr/bin/env bash
+
+ICON_DIR="/run/current-system/sw/share/icons/Papirus-Dark/24x24/symbolic/status/"
+
+notify_event() {
+    local icon="$1"
+    local title="$2"
+    local message="$3"
+
+    notify-send \
+        -e \
+        -u low \
+        -h boolean:SWAYNC_BYPASS_DND:true \
+        -i "$ICON_DIR/$icon" \
+        "$title" "$message"
+}
+
+if [[ "$1" == "network" ]]; then
+    case "$2" in
+        connected)
+            notify_event "network-transmit-receive-symbolic.svg" \
+                "Network" "Connected: $3"
+            ;;
+        disconnected)
+            notify_event "network-offline-symbolic.svg" \
+                "Network" "Disconnected"
+            ;;
+    esac
+fi
+
+if [[ "$1" == "bluetooth" ]]; then
+    case "$2" in
+        connected)
+            notify_event "bluetooth-active-symbolic.svg" \
+                "Bluetooth" "Connected: $3"
+            ;;
+        disconnected)
+            notify_event "bluetooth-disabled-symbolic.svg" \
+                "Bluetooth" "Disconnected"
+            ;;
+    esac
+fi
