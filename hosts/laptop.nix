f65f18b7b32317f17dd37b1fd47433e49c6ab13a
Benzenec6h6 <aconitinec34h47no11@gmail.com>
2025-12-04 21:05:52 +0900

    change opengl graphics

--- hosts/laptop.nix
+++ hosts/laptop.nix
@@ -21,7 +21,7 @@
   ];
 
   services.xserver.videoDrivers = [ "nvidia" ];
-  hardware.opengl.enable = true;
+  hardware.graphics.enable = true;
   hardware.nvidia = {
     open = true;
     modesetting.enable = true;
