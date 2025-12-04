7508e4864999dc84005249b5096d0e164fb84e06
Benzenec6h6 <aconitinec34h47no11@gmail.com>
2025-12-04 16:03:45 +0900

    25.05 25.11

--- flake.nix
+++ flake.nix
@@ -2,15 +2,15 @@
   description = "TetoOS - NixOS + Home Manager + Stylix";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.11";
 
     home-manager = {
-      url = "github:nix-community/home-manager/release-25.05";
+      url = "github:nix-community/home-manager/release-25.11";
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
     stylix = {
-      url = "github:danth/stylix/release-25.05";
+      url = "github:danth/stylix/release-25.11";
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
