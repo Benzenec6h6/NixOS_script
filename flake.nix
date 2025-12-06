c3ff895549d88f19564c99f086681b852e72dd44
Benzenec6h6 <aconitinec34h47no11@gmail.com>
2025-12-06 14:51:04 +0900

    delete nur

--- flake.nix
+++ flake.nix
@@ -22,17 +22,13 @@
 
     zen-browser.url = "github:MarceColl/zen-browser-flake";
 
-    nur.url = "github:nix-community/NUR";
   };
 
-  outputs = { nixpkgs, home-manager, stylix, zen-browser, nur, ... }@inputs:
+  outputs = { nixpkgs, home-manager, stylix, zen-browser, ... }@inputs:
     let
       system = "x86_64-linux";
       username = "teto";
       userPassword = "$6$qPo6ahBPqNC7mMim$WupFSLamdZfSEoafxSoE1ODgtaHS8gmUayQ2dTiW4vDBAVVJDcuj1yMYAHq.tz5mmZW7aqb44KnMacSq12xpO1";
-      overlays = [
-        nur.overlay
-      ];
 
       mkNixosConfig = profile: nixpkgs.lib.nixosSystem {
         inherit system;
@@ -40,9 +36,6 @@
           inherit inputs username profile userPassword;
         };
         modules = [
-          {
-            nixpkgs.overlays = overlays;
-          }
           ./hosts/${profile}.nix
         ];
       };
