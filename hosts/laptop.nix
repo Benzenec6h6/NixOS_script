de6d8396f9b358be30dc4e5ee2451346b72e2c6e
Benzenec6h6 <aconitinec34h47no11@gmail.com>
2025-12-08 16:00:37 +0900

    change nvidia driver stable

--- hosts/laptop.nix
+++ hosts/laptop.nix
@@ -31,7 +31,7 @@
     modesetting.enable = true;
     powerManagement.enable = false;
     powerManagement.finegrained = false;
-    package = config.boot.kernelPackages.nvidiaPackages.production;
+    package = config.boot.kernelPackages.nvidiaPackages.stable;
     prime = {
       intelBusId = ""; #"PCI:0:2:0"は固有の番号なのでpciutilsで調べる
       nvidiaBusId = ""; #ここも同様
