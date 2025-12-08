53aec4635bfe69a88281fbc66ef8b1001b6659c5
Benzenec6h6 <aconitinec34h47no11@gmail.com>
2025-12-08 18:27:17 +0900

    fixed network settings

--- modules/core/networking.nix
+++ modules/core/networking.nix
@@ -4,7 +4,26 @@
   networking.hostName = "nixos"; # hosts/*.nix で上書きしても良い
 
   # NetworkManager を有効化
-  networking.networkmanager.enable = true;
+  networking.networkmanager = {
+    enable = true;
+
+    # DNS を systemd-resolved に任せる（最も安定）
+    dns = "systemd-resolved";
+
+    # 接続性チェック（キャプティブポータル検知）
+    settings = {
+      connectivity = {
+        enabled = true;
+        #uri = "http://nmcheck.gnome.org/check_network_status.txt";
+        uri = "https://nixos.org/";
+        response = "OK";
+        interval = 5;
+      };
+    };
+  };
+
+  services.resolved.enable = true;
+  networking.enableIPv6 = false;
 
   # Firewall 設定 (ufw or nftables のどちらか)
   networking.firewall = {
