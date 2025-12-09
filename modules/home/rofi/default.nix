0fb7035603151221c19e4ff5c578cc306ec87e11
Benzenec6h6 <aconitinec34h47no11@gmail.com>
2025-12-09 22:51:58 +0900

    new rofi config

--- modules/home/rofi/default.nix
+++ modules/home/rofi/default.nix
@@ -1,6 +1,25 @@
-{ ... }: {
-  imports = [
-    ./rofi-theme.nix
-    ./rofi.nix
-  ];
+{ config, pkgs, ... }:
+
+{
+    programs.rofi = {
+        enable = true;
+        package = pkgs.rofi;
+
+        plugins = [
+            pkgs.rofi-calc
+            pkgs.rofi-mpd
+            pkgs.rofi-screenshot
+            pkgs.rofi-network-manager
+            pkgs.rofi-bluetooth
+            pkgs.todofi-sh
+        ];
+
+        extraConfig = {
+            modi = "drun,run,window,calc,bluetooth";
+            show-icons = true;
+        };
+
+    };
+
+    home.file.".config/rofi".source = ./rasi;
 }
