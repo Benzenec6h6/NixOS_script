037f2d010741792e2dfd6db6f9099e3c85f0dfd8
Benzenec6h6 <aconitinec34h47no11@gmail.com>
2025-12-10 00:12:57 +0900

    enable rofi

--- modules/home/stylix.nix
+++ modules/home/stylix.nix
@@ -2,7 +2,7 @@ _: {
   stylix.targets = {
     gtk.enable = true;
     waybar.enable = false;
-    rofi.enable = false;
+    rofi.enable = true;
     hyprland.enable = false;
     hyprlock.enable = false;
     starship.enable = false;
