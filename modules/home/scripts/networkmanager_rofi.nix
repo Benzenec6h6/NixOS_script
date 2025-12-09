46078629cef05086a5774245f30dd92ef238a4a4
Benzenec6h6 <aconitinec34h47no11@gmail.com>
2025-12-09 23:04:36 +0900

    forget pkgs

--- modules/home/scripts/networkmanager_rofi.nix
+++ modules/home/scripts/networkmanager_rofi.nix
@@ -1,3 +1,5 @@
+{ pkgs }:
+
 pkgs.writeShellScriptBin "networkmanager_rofi" ''
   export PATH=${pkgs.networkmanager}/bin:${pkgs.rofi}/bin:${pkgs.networkmanager_dmenu}/bin:${pkgs.gnugrep}/bin:$PATH
 
