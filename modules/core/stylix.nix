dbd0d18b0098c596d57c2f9af5f52a59d7d1b29f
Benzenec6h6 <aconitinec34h47no11@gmail.com>
2025-12-11 19:00:04 +0900

    return base16Scheme settings

--- modules/core/stylix.nix
+++ modules/core/stylix.nix
@@ -10,8 +10,8 @@
     polarity = "dark";
 
     #テーマ設定
-    base16Scheme = pkgs.base16-schemes.tokyo-night-dark;
-    #base16Scheme = "${pkgs.base16-schemes}/share/themes/tokyo-night-dark.yaml";
+    #base16Scheme = pkgs.base16-schemes.tokyo-night-dark;
+    base16Scheme = "${pkgs.base16-schemes}/share/themes/tokyo-night-dark.yaml";
     /*
     base16Scheme = {
       base00 = "282936";
