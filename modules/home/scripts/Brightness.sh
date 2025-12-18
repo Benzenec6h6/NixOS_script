272930b8d80b59ca7d6a0ee898328ce414104934
Benzenec6h6 <aconitinec34h47no11@gmail.com>
2025-12-19 00:17:40 +0900

    separate nix settings

---
+++ modules/home/scripts/Brightness.sh
@@ -0,0 +1,55 @@
+#!/usr/bin/env bash
+
+  STEP_NORMAL=5
+  STEP_FINE=1
+
+  ICON="display-brightness-high-symbolic"
+
+  get_brightness() {
+      brightnessctl -m | cut -d, -f4 | tr -d '%'
+  }
+
+  send_notification() {
+      local brightness=$1
+      notify-send -e \
+          --icon="$ICON" \
+          -h string:x-canonical-private-synchronous:brightness_notif \
+          -h int:value:"$brightness" \
+          -u low \
+          "Brightness: ''${brightness}%"
+  }
+
+  change_brightness() {
+      local delta=$1
+      local current new
+
+      current=$(get_brightness)
+      new=$((current + delta))
+
+      (( new < 0 )) && new=0
+      (( new > 100 )) && new=100
+
+      brightnessctl set "''${new}%"
+      send_notification "$new"
+  }
+
+  case "$1" in
+      "--get")
+          get_brightness
+          ;;
+      "--inc")
+          change_brightness "$STEP_NORMAL"
+          ;;
+      "--dec")
+          change_brightness "-$STEP_NORMAL"
+          ;;
+      "--inc-fine")
+          change_brightness "$STEP_FINE"
+          ;;
+      "--dec-fine")
+          change_brightness "-$STEP_FINE"
+          ;;
+      *)
+          echo "Usage: $0 [--inc|--dec|--inc-fine|--dec-fine|--get]"
+          ;;
+  esac
