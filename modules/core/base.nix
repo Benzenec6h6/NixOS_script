1b9a31a02bbe8e6fb979d271a1cd59fc0912b505
Benzenec6h6 <aconitinec34h47no11@gmail.com>
2025-12-18 19:15:48 +0900

    add nixos settings

--- modules/core/base.nix
+++ modules/core/base.nix
@@ -7,8 +7,27 @@
   
   console.keyMap = "jp106";
 
+  boot.loader.grub.configurationLimit = 5;
+
   nix = {
     package = pkgs.nixVersions.stable;
     settings.experimental-features = [ "nix-command" "flakes" ];
   };
+
+  nix.settings = {
+    max-jobs = "auto";
+    cores = 0;
+    auto-optimise-store = false; # optimiseと二重にしない
+  };
+
+  services.tmpfiles.clean.enable = true;
+
+  services.journald.extraConfig = ''
+    SystemMaxUse=512M
+    SystemMaxFiles=200
+    MaxRetentionSec=14day
+  '';
+
+  services.fstrim.enable = true;
+
 }
