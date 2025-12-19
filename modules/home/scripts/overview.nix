cc368c6d3bffb918e69330fc6a5b77bc8fdb45fa
Benzenec6h6 <aconitinec34h47no11@gmail.com>
2025-12-19 18:58:56 +0900

    Not in use

--- modules/home/scripts/overview.nix
+++
@@ -1,9 +0,0 @@
-{ pkgs, inputs, ... }:
-pkgs.writeShellScriptBin "overview" ''
-  export HYPRLAND_INSTANCE_SIGNATURE=$(cat /tmp/hypr/$(whoami)*/.instance)
-  if pgrep -f quickshell.*overview.qml >/dev/null; then
-    pkill -f quickshell.*overview.qml
-  else
-    ${inputs.quickshell.packages.${pkgs.system}.default}/bin/quickshell -p $HOME/.config/quickshell/overview.qml &
-  fi
-''
