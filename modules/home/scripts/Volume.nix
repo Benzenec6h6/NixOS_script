062be46dfd30ef7a7d6dbb33c2014425a13acca2
Benzenec6h6 <aconitinec34h47no11@gmail.com>
2025-12-22 23:28:04 +0900

    dependence alsa-utils

--- modules/home/scripts/Volume.nix
+++ modules/home/scripts/Volume.nix
@@ -4,7 +4,7 @@ pkgs.writeShellScriptBin "volume" ''
   #!${pkgs.bash}/bin/bash
 
   # 必要依存をすべて PATH に追加
-  export PATH=${pkgs.pamixer}/bin:${pkgs.libnotify}/bin:${pkgs.wireplumber}/bin:${pkgs.pipewire}/bin:$PATH
+  export PATH=${pkgs.pamixer}/bin:${pkgs.libnotify}/bin:${pkgs.wireplumber}/bin:${pkgs.pipewire}/bin:${pkgs.alsa-utils}/bin:$PATH
 
   ${builtins.readFile ./Volume.sh}
 ''
