7f3ff8e0f7ebf6e4573b68aeb2cc5a544d60b750
Benzenec6h6 <aconitinec34h47no11@gmail.com>
2026-01-06 16:33:47 +0900

    add extentions

--- modules/home/firefox.nix
+++ modules/home/firefox.nix
@@ -3,6 +3,22 @@
 {
     programs.firefox ={
         enable=ture;
-        
+
+        profiles.main = {
+            isDefault = true;
+
+            extensions.packages =
+                with pkgs.nur.repos.rycee.firefox-addons; [
+                    ublock-origin
+                    tridactyl
+                    darkreader
+                    simple-translate
+                    multi-account-containers
+                    #cookies-txt 一応保留,yt-dlpと相性が良さそう
+                    #enhancer-for-youtube
+                    #improved-tube
+                    #pockettube 検索でヒットしない。
+                ];
+        };
     }
 }
