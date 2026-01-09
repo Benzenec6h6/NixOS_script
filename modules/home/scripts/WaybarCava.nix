442bcb8bb48484d0e7a1d8659fc5ac7e49bdecbb
Benzenec6h6 <aconitinec34h47no11@gmail.com>
2026-01-10 00:30:08 +0900

    separate waybarcava nix config

--- modules/home/scripts/WaybarCava.nix
+++ modules/home/scripts/WaybarCava.nix
@@ -1,34 +1,17 @@
 { pkgs, ... }:
 
 let
-  # cavaの設定ファイルをNix側で定義（型安全でクリーン）
-  cavaConfig = pkgs.writeText "waybar-cava.conf" ''
-    [general]
-    framerate = 30
-    bars = 10
-
-    [input]
-    method = pulse
-    source = auto
-
-    [output]
-    method = raw
-    raw_target = /dev/stdout
-    data_format = ascii
-    ascii_max_range = 7
-  '';
+  # 同一ディレクトリにあるファイルを Nix ストアにコピーしてパスを取得
+  cavaConfig = ./waybar-cava.conf;
 
   waybarCava = pkgs.writeShellApplication {
     name = "WaybarCava";
     runtimeInputs = [ pkgs.cava pkgs.gnused pkgs.procps pkgs.coreutils ];
     text = ''
-      # Nixで作成した設定ファイルのパスを環境変数で渡す
+      # Nix ストア上の設定ファイルパスを環境変数にセット
       export CAVA_CONFIG_PATH="${cavaConfig}"
       ${builtins.readFile ./WaybarCava.sh}
     '';
   };
 in
-{
-  # Waybarのカスタムモジュールから呼び出せるようにパッケージに追加
-  home.packages = [ waybarCava ];
-}
+waybarCava
