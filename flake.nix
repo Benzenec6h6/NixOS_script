ee92038516a76cd38af2579afd566b96686fdb3d
Benzenec6h6 <aconitinec34h47no11@gmail.com>
2026-01-16 18:41:15 +0900

    add inputs disko

--- flake.nix
+++ flake.nix
@@ -5,6 +5,11 @@
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.11";
     #nixpkgs-unstable.url = "github:NixOS/nixpkgs/nixos-unstable";
 
+    disko = {
+      url = "github:nix-community/disko";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
     home-manager = {
       url = "github:nix-community/home-manager/release-25.11";
       inputs.nixpkgs.follows = "nixpkgs";
