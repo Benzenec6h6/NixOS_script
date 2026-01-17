cc057062567a61d714f84eeb6521422907c2bed5
Benzenec6h6 <aconitinec34h47no11@gmail.com>
2026-01-17 17:02:25 +0900

    fixed syntax

--- hosts/vm/disko.nix
+++ hosts/vm/disko.nix
@@ -1,50 +1,48 @@
-{ ... }:
+{ device, ... }:
 
 {
-  disko.devices = {
-    disk.main = {
-      device = "/dev/vda";   # virtio 前提
-      type = "disk";
+  disko.devices.disk.main = {
+    device = device;
+    type = "disk";
 
-      content = {
-        type = "gpt";
-        partitions = {
-          esp = {
-            size = "256M";
-            type = "EF00";
-            content = {
-              type = "filesystem";
-              format = "vfat";
-              mountpoint = "/boot";
-            };
+    content = {
+      type = "gpt";
+      partitions = {
+        esp = {
+          size = "256M";
+          type = "EF00";
+          content = {
+            type = "filesystem";
+            format = "vfat";
+            mountpoint = "/boot";
           };
+        };
 
-          root = {
-            size = "100%";
-            content = {
-              type = "btrfs";
-              extraArgs = [ "-f" ]; # 上書きを許可
-              subvolumes = {
-                "@root" = { 
-                  mountpoint = "/";
-                  mountOptions = [ "compress=zstd" "noatime" ];
-                };
-                "@nix"  = { 
-                  mountpoint = "/nix";
-                  mountOptions = [ "compress=zstd" "noatime" ];
-                };
-                "@home" = { 
-                  mountpoint = "/home";
-                  mountOptions = [ "compress=zstd" "noatime" ];
-                };
-                "@persist" = { 
-                  mountpoint = "/persist";
-                  mountOptions = [ "compress=zstd" "noatime" ];
-                };
-                "@swap" = {
-                  mountpoint = "/.swapvol";
-                  swap.swapfile.size = "4G"; # スワップファイルが必要な場合
-                };
+        root = {
+          size = "100%";
+          content = {
+            type = "btrfs";
+            extraArgs = [ "-f" ]; # 上書きを許可
+            subvolumes = {
+              "@root" = { 
+                mountpoint = "/";
+                mountOptions = [ "compress=zstd" "noatime" ];
+              };
+              "@nix"  = { 
+                mountpoint = "/nix";
+                mountOptions = [ "compress=zstd" "noatime" ];
+              };
+              "@home" = { 
+                mountpoint = "/home";
+                mountOptions = [ "compress=zstd" "noatime" ];
+              };
+              "@persist" = { 
+                mountpoint = "/persist";
+                mountOptions = [ "compress=zstd" "noatime" ];
+              };
+              "@swap" = {
+                mountpoint = "/.swapvol";
+                swap.swapfile.size = "4G"; # スワップファイルが必要な場合
               };
             };
           };
