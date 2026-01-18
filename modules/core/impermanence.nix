bbb971b9416964812a7dc25da8f69057d4c29014
Benzenec6h6 <aconitinec34h47no11@gmail.com>
2026-01-19 01:29:05 +0900

    add inputs fuse settings

--- modules/core/impermanence.nix
+++ modules/core/impermanence.nix
@@ -1,6 +1,7 @@
-{ vars, ... }:
+{ inputs, vars, ... }:
 
 {
+  programs.fuse.userAllowOther = true;
   # システム全体の永続化
   environment.persistence."/persist" = {
     hideMounts = true; # マウントを隠してディレクトリを綺麗に保つ
