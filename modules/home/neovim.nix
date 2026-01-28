7c71c2dfd61b38b643217d5e62542f3735962202
Benzenec6h6 <aconitinec34h47no11@gmail.com>
2026-01-28 17:36:04 +0900

    add plugins

--- modules/home/neovim.nix
+++ modules/home/neovim.nix
@@ -7,14 +7,66 @@
     viAlias = true;
     vimAlias = true;
 
+    # 1. プラグインの追加
+    plugins = with pkgs.vimPlugins; [
+      nvim-tree-lua
+      nvim-web-devicons # アイコンを表示するために推奨
+      (nvim-treesitter.withPlugins (p: with p; [
+        # 必須・基本
+        nix
+        lua
+        vim
+        vimdoc
+        query # Treesitter自体のデバッグ用
+
+        # Web / 設定ファイル (よく遭遇するもの)
+        html
+        css
+        javascript
+        typescript
+        json
+        yaml
+        toml
+        markdown
+        markdown_inline
+
+        # 自分がいつか書くかもしれない言語 (お好みで)
+        python
+        bash
+        rust
+        go
+        dockerfile
+      ]))
+    ];
+
     extraConfig = ''
-        set number
-        set relativenumber
-        set expandtab
-        set shiftwidth=2
-        set tabstop=2
-        set smartindent
-        set clipboard=unnamedplus
+      set number
+      set relativenumber
+      set expandtab
+      set shiftwidth=2
+      set tabstop=2
+      set smartindent
+      set clipboard=unnamedplus
     '';
-    };
+
+    # 2. Luaによるプラグインの設定
+    extraLuaConfig = ''
+      -- nvim-treeの初期化
+      require("nvim-tree").setup({
+        sort_by = "case_sensitive",
+        view = {
+          width = 30,
+        },
+        renderer = {
+          group_empty = true,
+        },
+        filters = {
+          dotfiles = false,
+        },
+      })
+
+      -- キーマッピング (例: Ctrl+n でツリーを開閉)
+      vim.keymap.set('n', '<C-n>', ':NvimTreeToggle<CR>', { silent = true })
+    '';
+  };
 }
