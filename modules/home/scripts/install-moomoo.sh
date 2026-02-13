013b1bd54193665bfca3f290af37c3fced5036ce
Benzenec6h6 <aconitinec34h47no11@gmail.com>
2026-02-14 07:41:39 +0900

    Change destination

--- modules/home/scripts/install-moomoo.sh
+++ modules/home/scripts/install-moomoo.sh
@@ -3,16 +3,17 @@ set -e
 
 DEB_SRC="@moomooDeb@"
 # _aptユーザもアクセスできる /tmp にコピーする
-TMP_DEB="/tmp/moomoo_install.deb"
+TMP_DEB="$HOME/moomoo_install.deb"
 
 echo "Copying deb to /tmp..."
 cp "$DEB_SRC" "$TMP_DEB"
 chmod 644 "$TMP_DEB" # 誰でも読めるように権限付与
+ls -l "$TMP_DEB"
 
 echo "Creating Distrobox container..."
 distrobox create --name moomoo --image ubuntu:22.04 --yes
 
-echo "Installing moomoo inside container..."
+echo "Ensure dependencies are aligned..."
 distrobox enter moomoo -- bash -c "
   sudo apt update && \
   sudo apt install -y \
@@ -20,7 +21,11 @@ distrobox enter moomoo -- bash -c "
     libxcb-render-util0 libxcb-xinerama0 libxcb-cursor0 libxcb-icccm4 \
     libxcb-image0 libxcb-keysyms1 libxcb-shape0 libxkbcommon-x11-0 \
     desktop-file-utils libgl1-mesa-glx \
-    wget gdebi-core && \
+    wget gdebi-core
+"
+
+echo "Installing moomoo inside container..."
+distrobox enter moomoo -- bash -c "
   sudo gdebi -n $TMP_DEB && \
   distrobox-export --app moomoo
 "
