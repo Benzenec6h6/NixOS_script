65b1fd140c6ce1025c0da9d876aa9835b8901b24
Benzenec6h6 <aconitinec34h47no11@gmail.com>
2026-02-17 19:19:40 +0900

    Remove non-existent options

--- modules/home/git.nix
+++ modules/home/git.nix
@@ -5,10 +5,6 @@
       name = vars.user.gitName;
       email = vars.user.gitEmail;
     };
-    config = {
-      credential.helper = "keepassxc";
-      # SSH鍵の署名を検証する場合などに備えて
-      # gpg.format = "ssh"; 
-    };
   };
 }
+
