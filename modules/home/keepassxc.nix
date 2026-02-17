2d2dcfe0f14824d6597009e8fcdc6bef07df3508
Benzenec6h6 <aconitinec34h47no11@gmail.com>
2026-02-17 16:29:07 +0900

    add keepassxc settings

---
+++ modules/home/keepassxc.nix
@@ -0,0 +1,16 @@
+{ pkgs, ... }:
+
+{
+  # 1. KeePassXC 本体の有効化
+  programs.keepassxc = {
+    enable = true;
+    autostart = true;
+  };
+
+  # 2. Git 認証連携の設定 (HTTPS用)
+  programs.git-credential-keepassxc.enable = true; 
+  #programs.git-credential-keepassxc.groups = [ "Development/GitHub" ];
+
+  # 3. SSH Agent の有効化 (KeePassXCがこれを利用します)
+  programs.ssh.startAgent = true;
+}
