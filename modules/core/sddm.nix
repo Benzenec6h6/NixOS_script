1c0a30ebb5190f988192699dcc4706a1a6a378fb
Benzenec6h6 <aconitinec34h47no11@gmail.com>
2026-03-18 03:11:38 +0900

    override theme

--- modules/core/sddm.nix
+++ modules/core/sddm.nix
@@ -1,17 +1,25 @@
 { config, pkgs, ... }:
 
 let
-  astronautTheme = pkgs.callPackage ./build.nix {};
+  # nixpkgsのsddm-astronautをベースに、引数だけを上書き
+  astronautTheme = pkgs.sddm-astronaut.override {
+    embeddedTheme = "pixel_sakura";
+    # 必要に応じて、ここでさらに詳細な設定（themeConfig）も可能です
+  };
 in
 {
   services.displayManager = {
     defaultSession = "hyprland-uwsm";
-    sddm ={
+    sddm = {
       enable = true;
       wayland.enable = true;
       package = pkgs.kdePackages.sddm;
 
-      theme = "sddm-astronaut-theme";
+      # テーマ名はnixpkgsの定義に従いこれを使います
+      theme = "sddm-astronaut";
+      
+      # 必要なQtパッケージは自動的に伝播（Propagate）されますが、
+      # 明示的に追加しておく場合もこちらで大丈夫です
       extraPackages = with pkgs.kdePackages; [
         qtmultimedia
         qtvirtualkeyboard
@@ -20,9 +28,8 @@ in
     };
   };
 
-  environment.systemPackages = with pkgs; [
-    kdePackages.qtbase
-    astronautTheme 
-    #sddm-astronaut
+  environment.systemPackages = [
+    pkgs.kdePackages.qtbase
+    astronautTheme # 上記で定義したカスタムテーマ
   ];
 }
