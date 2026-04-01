fa2c3eebe6002a49dddb37b26170bb1683411dcf
Benzenec6h6 <aconitinec34h47no11@gmail.com>
2026-04-02 03:22:43 +0900

    for sops-nix

--- modules/core/users.nix
+++ modules/core/users.nix
@@ -1,7 +1,35 @@
-{ pkgs, inputs, vars, ... }:
+{ config, pkgs, inputs, vars, ... }:
 
 {
-  imports = [ inputs.home-manager.nixosModules.home-manager ];
+  imports = [ 
+    inputs.home-manager.nixosModules.home-manager
+    inputs.sops-nix.homeManagerModules.sops
+  ];
+
+  sops = {
+    # 秘密情報ファイルの場所（Flakeルートからの相対パスなど）
+    defaultSopsFile = ../../secrets/secrets.yaml;
+    age.sshKeyPaths = [ "/etc/ssh/ssh_host_ed25519_key" ];
+    # 復号したファイルをどこに置くか
+    secrets = {
+      "password" = {
+        neededForUsers = true;
+      };
+      "location/city" = { };
+      "location/lat" = { };
+      "location/lon" = { };
+      "api-key" = { };
+    };
+    templates."weather-env" = {
+      owner = vars.user.name;
+      content = ''
+        CITY_NAME="${config.sops.placeholder."location/city"}"
+        LAT="${config.sops.placeholder."location/lat"}"
+        LON="${config.sops.placeholder."location/lon"}"
+        OWM_KEY="${config.sops.placeholder."api-key"}"
+      '';
+    };
+  };
 
   home-manager = {
     useUserPackages = true;
@@ -20,7 +48,7 @@
     isNormalUser = true;
     description = "Main user";
     extraGroups = [ "wheel" "networkmanager" "libvirtd" "scanner" "lp" "video" "input"  "audio" "docker" "libvirtd" "kvm" ];
-    hashedPassword = vars.user.password;
+    hashedPasswordFile = config.sops.secrets.user-password.path;
     shell = pkgs.zsh;
     ignoreShellProgramCheck = true;
   };
