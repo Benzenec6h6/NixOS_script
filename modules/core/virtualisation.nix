db6384d97548e1fe7f1e856f82deeb1764c328ce
Benzenec6h6 <aconitinec34h47no11@gmail.com>
2026-04-11 15:55:51 +0900

    enable dnsmasq for vm settings

--- modules/core/virtualisation.nix
+++ modules/core/virtualisation.nix
@@ -19,36 +19,38 @@
     settings.dynamic_tuning = true;
   };
   
-  #services.dnsmasq = {
-  #  enable = true;
+  services.dnsmasq = {
+    enable = true;
     
     # 1. このVM自身もdnsmasqをDNSサーバーとして使うようにする
-  #  resolveLocalQueries = true;
+    resolveLocalQueries = true;
 
     # 2. 詳細な挙動をここに集約して記述する
-  #  settings = {
-      # 上位DNSサーバーの指定（GoogleとCloudflareの例）
-  #    server = [ "8.8.8.8" "1.1.1.1" ];
+    settings = {
+      resolv-file = "/run/systemd/resolve/resolv.conf";
 
+      proxy-dnssec = true;
       # キャッシュするレコード数（デフォルト150を少し増やして効率化）
-  #    cache-size = 1000;
+      cache-size = 1000;
 
       # 不正なドメイン名（ドットを含まない名前）を上位に転送しない
-  #    domain-needed = true;
+      domain-needed = true;
 
       # プライベートIPの逆引きを上位に転送しない
-  #    bogus-priv = true;
+      bogus-priv = true;
 
       # ローカルネットワーク内での名前解決（例：my-vm.local）
-  #    local = "/local/";
-  #    domain = "local";
-  #    expand-hosts = true;
+      local = "/local/";
+      domain = "local";
+      expand-hosts = true;
 
+      interface = [ "lo" "virbr0" ];
+      bind-interfaces = true;
       # ログ設定（トラブルシューティング時に便利。不要ならコメントアウト）
-      # log-queries = true;
-      # log-facility = "/var/log/dnsmasq.log";
-  #  };
-  #};
+       log-queries = true;
+       log-facility = "/var/log/dnsmasq.log";
+    };
+  };
 
   # 3. 監視も行いたい場合（Prometheus exporter）
   # services.prometheus.exporters.dnsmasq = {
