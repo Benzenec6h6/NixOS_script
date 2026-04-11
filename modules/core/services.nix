886f2e7e8bb1942454e64688522b1d9851ec0959
Benzenec6h6 <aconitinec34h47no11@gmail.com>
2026-04-11 14:03:08 +0900

    verbatim config

--- modules/core/services.nix
+++ modules/core/services.nix
@@ -64,6 +64,10 @@
     qemu = {
       package = pkgs.qemu_full; # GUI等フル機能版のQEMU
       swtpm.enable = true;      # TPMエミュレータを有効化
+      verbatimConfig = ''
+        user = "teto"  # あなたのUID 1000のユーザー名
+        group = "libvirtd"
+      '';
     };
   };
 
