3220d945d268efb972128ff3a41d06b99c460191
Benzenec6h6 <aconitinec34h47no11@gmail.com>
2026-04-17 16:39:53 +0900

    Apply luks

--- hosts/laptop/disko.nix
+++ hosts/laptop/disko.nix
@@ -15,38 +15,46 @@
             type = "filesystem";
             format = "vfat";
             mountpoint = "/boot";
+            mountOptions = [ "umask=0077" ];
           };
         };
 
-        root = {
+        luks = {
           size = "100%";
           content = {
-            type = "btrfs";
-            extraArgs = [ "-f" ];
-            subvolumes = {
-              "@root" = {
-                mountpoint = "/";
-                mountOptions = [ "compress=zstd" "noatime" ];
-              };
+            type = "luks";
+            name = "crypted";
+            settings = {
+              allowDiscards = true;
+            };
+            content = {
+              type = "btrfs";
+              extraArgs = [ "-f" ];
+              subvolumes = {
+                "@root" = {
+                  mountpoint = "/";
+                  mountOptions = [ "compress=zstd" "noatime" ];
+                };
 
-              "@nix" = {
-                mountpoint = "/nix";
-                mountOptions = [ "compress=zstd" "noatime" ];
-              };
+                "@nix" = {
+                  mountpoint = "/nix";
+                  mountOptions = [ "compress=zstd" "noatime" ];
+                };
 
-              "@home" = {
-                mountpoint = "/home";
-                mountOptions = [ "compress=zstd" "noatime" ];
-              };
+                "@home" = {
+                  mountpoint = "/home";
+                  mountOptions = [ "compress=zstd" "noatime" ];
+                };
 
-              "@persist" = {
-                mountpoint = "/persist";
-                mountOptions = [ "compress=zstd" "noatime" ];
-              };
+                "@persist" = {
+                  mountpoint = "/persist";
+                  mountOptions = [ "compress=zstd" "noatime" ];
+                };
 
-              "@swap" = {
-                mountpoint = "/.swapvol";
-                swap.swapfile.size = "8G";
+                "@swap" = {
+                  mountpoint = "/.swapvol";
+                  swap.swapfile.size = "8G";
+                };
               };
             };
           };
