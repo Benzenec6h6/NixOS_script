a31a5983d7a1b5fd723bc883b467c71faf761fca
Benzenec6h6 <aconitinec34h47no11@gmail.com>
2026-04-20 06:17:26 +0900

    update hardware.nix

--- hosts/laptop/hardware.nix
+++ hosts/laptop/hardware.nix
@@ -13,13 +13,6 @@
   boot.kernelModules = [ "kvm-intel" ];
   boot.extraModulePackages = [ ];
 
-
-
-
-
-
-
-
   nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
   hardware.cpu.intel.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
 }
