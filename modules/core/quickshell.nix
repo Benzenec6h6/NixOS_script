aee30b8d3dfee6e84a88bb3761f33b874214a588
Benzenec6h6 <aconitinec34h47no11@gmail.com>
2026-04-22 17:47:02 +0900

    Changed to the recommended format

--- modules/core/quickshell.nix
+++ modules/core/quickshell.nix
@@ -1,7 +1,7 @@
-{ pkgs, inputs, ... }:
+{ pkgs, inputs, vars, ... }:
 {
   environment.systemPackages = with pkgs; [
-    inputs.quickshell.packages.${pkgs.system}.default
+    inputs.quickshell.packages.${vars.system}.default
 
     # Qt6 related kits（for slove Qt5Compat problem）
     qt6.qt5compat
