d31347541147dff87a7a6c15877bcbda01c4bf31
Benzenec6h6 <aconitinec34h47no11@gmail.com>
2026-05-07 19:05:38 +0900

    Changed from daily builds to weekly updates

--- modules/core/flatpak.nix
+++ modules/core/flatpak.nix
@@ -8,7 +8,21 @@
       "com.github.tchx84.Flatseal"
       "io.github.dvlv.boxbuddyrs"
     ];
-    update.onActivation = true;
+    update.onActivation = false;
+    update.auto = {
+      enable = true;
+      onCalendar = "weekly";
+    };
+
+    restartOnFailure = {
+      enable = true;
+      restartDelay = "10s"; # 1分待つのは長いので10秒にするなど
+      # 必要に応じて指数バックオフを有効化
+      exponentialBackoff = {
+        enable = true;
+        maxDelay = "5m";
+      };
+    };
   };
 
   # nix-flatpak が生成するサービスの設定を補強
@@ -16,10 +30,5 @@
     # ネットワークが完全にオンラインになるのを待つ
     after = [ "network-online.target" ];
     wants = [ "network-online.target" ];
-    serviceConfig = {
-      # 失敗しても10秒おきにリトライ（これでリビルドエラーを防ぐ）
-      Restart = lib.mkForce "on-failure";
-      RestartSec = lib.mkForce "10s";
-    };
   };
 }
