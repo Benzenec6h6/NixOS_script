f4b005b3693324f504bad046285076a95a26212e
Benzenec6h6 <aconitinec34h47no11@gmail.com>
2026-06-05 20:40:54 +0900

    Correcting the names of dependent packages

--- modules/home/scripts/WaybarScripts.nix
+++ modules/home/scripts/WaybarScripts.nix
@@ -1,26 +1,32 @@
-{ pkgs, vars, ... }:
-let
+{
+  pkgs,
+  vars,
+  ...
+}: let
   # 選択されたターミナルパッケージを特定
-  termPkg = if vars.user.terminal == "kitty" then pkgs.kitty 
-          else if vars.user.terminal == "ghostty" then pkgs.ghostty
-          else pkgs.foot;
+  termPkg =
+    if vars.user.terminal == "kitty"
+    then pkgs.kitty
+    else if vars.user.terminal == "ghostty"
+    then pkgs.ghostty
+    else pkgs.foot;
   # 実行コマンド名
   termCmd = vars.user.terminal;
 in
-pkgs.writeShellApplication {
-  name = "WaybarScripts";
-  runtimeInputs = [
-    termPkg
-    pkgs.btop
-    pkgs.nvtopPackages.full
-    pkgs.networkmanager
-    pkgs.xfce.thunar
-    pkgs.libnotify
-    pkgs.coreutils
-  ];
-  text = ''
-    # shellcheck disable=SC2034
-    termcmd="${termCmd}"
-    builtins.readFile ./WaybarScripts.sh
-  '';
-}
+  pkgs.writeShellApplication {
+    name = "WaybarScripts";
+    runtimeInputs = [
+      termPkg
+      pkgs.btop
+      pkgs.nvtopPackages.full
+      pkgs.networkmanager
+      pkgs.thunar
+      pkgs.libnotify
+      pkgs.coreutils
+    ];
+    text = ''
+      # shellcheck disable=SC2034
+      termcmd="${termCmd}"
+      builtins.readFile ./WaybarScripts.sh
+    '';
+  }
