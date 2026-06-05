aafc3d3e1eb0f18cfd57a87a88bb47f630ea2e0e
Benzenec6h6 <aconitinec34h47no11@gmail.com>
2026-06-05 17:54:03 +0900

    comment out niri settings

--- modules/home/default.nix
+++ modules/home/default.nix
@@ -1,40 +1,46 @@
-{ pkgs, vars, config, lib, ... }:
 {
-  imports = [
-    ./fastfetch
-    ./hyprland
-    ./mpv
-    ./neovim
-    ./rofi
-    ./scripts
-    ./starship
-    ./waybar
-    #./waybar/waybar-JaKooLit.nix
-    ./wlogout
-    ./apps.nix
-    #./brave.nix
-    ./cava.nix
-    ./cli-tools.nix
-    #./firefox.nix
-    ./fonts.nix
-    ./gh.nix
-    ./git.nix
-    ./hypridle.nix
-    ./impermanence.nix
-    ./keepassxc.nix
-    ./niri.nix
-    ./stylix.nix
-    ./swaync.nix
-    #./themes.nix
-    ./xdg.nix
-    ./yazi.nix
-    #./zellij.nix
-    ./zsh.nix
-  ] 
-  ++ (lib.optionals (vars.user.terminal == "kitty") [ ./kitty.nix ])
-  ++ (lib.optionals (vars.user.terminal == "ghostty") [ ./ghostty.nix ])
-  ++ (lib.optionals (vars.user.terminal == "foot") [ ./foot.nix ])
-  ++ (lib.optionals (vars.host == "laptop") [ ./ollama.nix ]);
+  pkgs,
+  vars,
+  config,
+  lib,
+  ...
+}: {
+  imports =
+    [
+      ./fastfetch
+      ./hyprland
+      ./mpv
+      ./neovim
+      ./rofi
+      ./scripts
+      ./starship
+      ./waybar
+      #./waybar/waybar-JaKooLit.nix
+      ./wlogout
+      ./apps.nix
+      #./brave.nix
+      ./cava.nix
+      ./cli-tools.nix
+      #./firefox.nix
+      ./fonts.nix
+      ./gh.nix
+      ./git.nix
+      ./hypridle.nix
+      ./impermanence.nix
+      ./keepassxc.nix
+      #./niri.nix
+      ./stylix.nix
+      ./swaync.nix
+      #./themes.nix
+      ./xdg.nix
+      ./yazi.nix
+      #./zellij.nix
+      ./zsh.nix
+    ]
+    ++ (lib.optionals (vars.user.terminal == "kitty") [./kitty.nix])
+    ++ (lib.optionals (vars.user.terminal == "ghostty") [./ghostty.nix])
+    ++ (lib.optionals (vars.user.terminal == "foot") [./foot.nix])
+    ++ (lib.optionals (vars.host == "laptop") [./ollama.nix]);
 
   programs.home-manager.enable = true;
 
