d9d79c1c17398a4471934453472ff3d62b72ee66
Benzenec6h6 <aconitinec34h47no11@gmail.com>
2026-06-05 20:19:53 +0900

    Changing the package name from swww to awww

--- modules/home/apps.nix
+++ modules/home/apps.nix
@@ -1,9 +1,14 @@
-{ config, pkgs, inputs, lib, vars, ... }:
-let
+{
+  config,
+  pkgs,
+  inputs,
+  lib,
+  vars,
+  ...
+}: let
   isVM = vars.host == "vm";
   isLaptop = vars.host == "laptop";
-in
-{
+in {
   programs.hyprlock.enable = true;
   programs.distrobox.enable = true;
   services.hypridle.enable = true;
@@ -12,7 +17,7 @@ in
   services.playerctld.enable = true;
   services.gnome-keyring = {
     enable = true;
-    components = [ "pkcs11" "secrets" "ssh" ];
+    components = ["pkcs11" "secrets" "ssh"];
   };
   #As of May 2026, there is a bug where `QWindow` remains `NULL` when calling `QmlDialogWrapperBase::show()`.
   #I guess we'll just have to wait for the upstream fix.
@@ -21,43 +26,51 @@ in
     forceWayland = true;
   };
 
-  home.packages = [
-    inputs.zen-browser.packages.${vars.system}.default
-    inputs.moomoo.packages.${vars.system}.default
-  ] ++ (with pkgs; [
-    vscodium
+  home.packages =
+    [
+      inputs.zen-browser.packages.${vars.system}.default
+      inputs.moomoo.packages.${vars.system}.default
+    ]
+    ++ (with pkgs;
+      [
+        vscodium
 
-    # 通信用
-    discord
+        # 通信用
+        discord
 
-    # ユーティリティ
-    steam
-    transmission_4-gtk
-    qbittorrent
-    #gparted
+        # ユーティリティ
+        steam
+        transmission_4-gtk
+        qbittorrent
+        #gparted
 
-    # 便利ツール
-    eww #ags
-    swww mpvpaper
-    pavucontrol #playerctl
-    imagemagick qimgv #notify-desktop
-    bottom btop nvtopPackages.full
-    qalculate-gtk
-    megacmd
+        # 便利ツール
+        eww #ags
+        awww
+        mpvpaper
+        pavucontrol #playerctl
+        imagemagick
+        qimgv
+        bottom
+        btop
+        nvtopPackages.full
+        qalculate-gtk
+        megacmd
 
-    #LSPサーバー
-    lua-language-server
-    nixd shfmt
-  ]
-  ++ lib.optionals isLaptop [
-      # Virtualization
-      #開発環境のflakeに閉じ込めた。
-      #virt-viewer virt-manager libosinfo swtpm
-      #optionを使った
-      #dnsmasq tuned 
-      #optionがない開発flakeに隔離する訳にもいかない
-      ntfs3g
+        #LSPサーバー
+        lua-language-server
+        nixd
+        shfmt
+      ]
+      ++ lib.optionals isLaptop [
+        # Virtualization
+        #開発環境のflakeに閉じ込めた。
+        #virt-viewer virt-manager libosinfo swtpm
+        #optionを使った
+        #dnsmasq tuned
+        #optionがない開発flakeに隔離する訳にもいかない
+        ntfs3g
 
-      kicad-small
-  ]);
+        kicad-small
+      ]);
 }
