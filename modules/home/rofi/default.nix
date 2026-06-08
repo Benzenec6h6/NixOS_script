f2103b567853c114c8c7a1e0dd2c95f18f05e36c
Benzenec6h6 <aconitinec34h47no11@gmail.com>
2026-06-08 19:54:43 +0900

    Make launches from rofi compliant with uwsm

--- modules/home/rofi/default.nix
+++ modules/home/rofi/default.nix
@@ -1,24 +1,31 @@
-{ config, pkgs, ... }:
-
 {
-    programs.rofi = {
-        enable = true;
-        package = pkgs.rofi;
+  config,
+  pkgs,
+  ...
+}: {
+  programs.rofi = {
+    enable = true;
+    cycle = true;
+    package = pkgs.rofi;
 
-        plugins = [
-            pkgs.rofi-calc
-            pkgs.rofi-mpd
-            pkgs.rofi-screenshot
-            #pkgs.rofi-network-manager
-            pkgs.rofi-bluetooth
-            pkgs.todofi-sh
-        ];
+    plugins = [
+      pkgs.rofi-calc
+      pkgs.rofi-mpd
+      pkgs.rofi-screenshot
+      #pkgs.rofi-network-manager
+      pkgs.rofi-bluetooth
+      pkgs.todofi-sh
+    ];
 
-        extraConfig = {
-            modi = "drun,run,window,calc";
-            show-icons = true; 
-        };
+    extraConfig = {
+      modi = "drun,run,window,calc";
+      show-icons = true;
+
+      run-command = "uwsm app -- {cmd}";
+      run-shell-command = "uwsm app -- {terminal} -e {cmd}";
+      drun-use-desktop-cache = false;
     };
+  };
 
-    home.file.".config/rofi/myconf".source = ./rasi;
+  #home.file.".config/rofi/myconf".source = ./rasi;
 }
