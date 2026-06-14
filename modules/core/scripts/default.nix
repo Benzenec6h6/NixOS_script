b4304ebe1896af3a449f263be8f2afe69a89fcf9
Benzenec6h6 <aconitinec34h47no11@gmail.com>
2026-06-14 20:04:16 +0900

    Import the storage and Wi-Fi scripts

--- modules/core/scripts/default.nix
+++ modules/core/scripts/default.nix
@@ -1,6 +1,8 @@
-{ pkgs, ... }:
-
-{
+{pkgs, ...}: {
+  imports = [
+    ./storage-monitor.nix
+    ./wifi-portal-manager.nix
+  ];
   environment.systemPackages = [
     (pkgs.callPackage ./hibernate-dynamic.nix {})
     (pkgs.callPackage ./resume-dynamic.nix {})
