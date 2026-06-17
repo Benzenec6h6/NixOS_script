63d64e71f38bedb2ef88148e974df84005125161
Benzenec6h6 <aconitinec34h47no11@gmail.com>
2026-06-17 18:48:33 +0900

    Start wifi-portal-manager as a systemd service

---
+++ modules/home/scripts/wifi-portal-manager.nix
@@ -0,0 +1,22 @@
+{pkgs, ...}: {
+  systemd.user.services.wifi-portal-watch = {
+    Unit = {
+      Description = "Wi-Fi Captive Portal Watcher";
+      # 1. グラフィカルセッションが整い
+      # 2. 通知サーバー(swaync)が立ち上がり
+      # 3. ネットワーク機能が利用可能になった後
+      After = ["graphical-session.target" "swaync.service" "network.target"];
+      PartOf = ["graphical-session.target"];
+    };
+
+    Service = {
+      ExecStart = "${pkgs.rust-tools}/bin/wifi-portal-watch";
+      Restart = "on-failure";
+      RestartSec = "5s";
+    };
+
+    Install = {
+      WantedBy = ["graphical-session.target"];
+    };
+  };
+}
