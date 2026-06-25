ef7e7f403edf24f7b868f89fcfe6c8e7cac092ca
Benzenec6h6 <aconitinec34h47no11@gmail.com>
2026-06-25 17:40:26 +0900

    Sort the cache servers by frequency of use, from highest to lowest

--- modules/core/base.nix
+++ modules/core/base.nix
@@ -20,18 +20,18 @@
     settings = {
       experimental-features = ["nix-command" "flakes"];
       substituters = [
-        "https://cache.tvl.fyi/"
-        "https://cache.lix.systems"
-        "https://attic.xuyh0120.win/lantian"
-        "https://cache.garnix.io"
         "https://cache.nixos.org"
+        "https://cache.garnix.io"
+        "https://attic.xuyh0120.win/lantian"
+        "https://cache.lix.systems"
+        "https://cache.tvl.fyi/"
       ];
       trusted-public-keys = [
-        "cache.tvl.su:kjc6KOMupXc1vHVufJUoDUYeLzbwSr9abcAKdn/U1Jk="
-        "cache.lix.systems:aBnZUw8zA7H35Cz2RyKFVs3H4PlGTLawyY5KRbvJR8o="
-        "lantian:EeAUQ+W+6r7EtwnmYjeVwx5kOGEBpjlBfPlzGlTNvHc="
-        "cache.garnix.io:CTFPyKSLcx5RMJKfLo5EEPUObbA78b0YQ2DTCJXqr9g="
         "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
+        "cache.garnix.io:CTFPyKSLcx5RMJKfLo5EEPUObbA78b0YQ2DTCJXqr9g="
+        "lantian:EeAUQ+W+6r7EtwnmYjeVwx5kOGEBpjlBfPlzGlTNvHc="
+        "cache.lix.systems:aBnZUw8zA7H35Cz2RyKFVs3H4PlGTLawyY5KRbvJR8o="
+        "cache.tvl.su:kjc6KOMupXc1vHVufJUoDUYeLzbwSr9abcAKdn/U1Jk="
       ];
       max-jobs = "auto";
       cores = 0;
