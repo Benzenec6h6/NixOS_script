4cec65cb508ef68a77ab9fac635c72da8f5b54b8
Benzenec6h6 <aconitinec34h47no11@gmail.com>
2026-06-29 19:15:34 +0900

    I don't use KeepassXC very often

--- modules/home/keepassxc.nix
+++ modules/home/keepassxc.nix
@@ -1,13 +1,12 @@
-{ pkgs, ... }:
-
-{
+{pkgs, ...}: {
   # 1. KeePassXC 本体の有効化
   programs.keepassxc = {
     enable = true;
-    autostart = true;
+    autostart = false;
   };
 
   # 2. Git 認証連携の設定 (HTTPS用)
-  programs.git-credential-keepassxc.enable = true; 
+  programs.git-credential-keepassxc.enable = true;
   #programs.git-credential-keepassxc.groups = [ "Development/GitHub" ];
 }
+
