42936786bead29a837696fbb10067de5149a75c4
Benzenec6h6 <aconitinec34h47no11@gmail.com>
2026-07-01 00:04:34 +0900

    Set it so that the home directory is also cleaned

--- hosts/laptop/rollback.sh
+++ hosts/laptop/rollback.sh
@@ -3,7 +3,6 @@ set -e
 mkdir -p /mnt-root
 mount /dev/mapper/crypted /mnt-root
 
-# 子サブボリュームを末端から順番に消していく安全な関数
 delete_subvolume_recursively() {
     IFS=$'\n'
     for i in $(btrfs subvolume list -o "$1" | cut -f 9- -d ' '); do
@@ -12,19 +11,25 @@ delete_subvolume_recursively() {
     btrfs subvolume delete "$1"
 }
 
-# 1. 既存の @root を @root_old にリネームして退避
+# --- @root のリセット ---
 if [ -e /mnt-root/@root ]; then
     mv /mnt-root/@root /mnt-root/@root_old
 fi
-
-# 2. 【重要】スナップショットではなく、完全に「空」のサブボリュームを新規作成する
-# これにより NixOS は起動時に「現在の世代」に合わせたシンボリックリンクを 
-# /nix/store からクリーンに作成します。
 btrfs subvolume create /mnt-root/@root
 
-# 3. 退避させた @root_old を削除
+# --- @home のリセット (追加) ---
+if [ -e /mnt-root/@home ]; then
+    mv /mnt-root/@home /mnt-root/@home_old
+fi
+btrfs subvolume create /mnt-root/@home
+
+# --- 古いサブボリュームの削除 ---
 if [ -e /mnt-root/@root_old ]; then
     delete_subvolume_recursively /mnt-root/@root_old
+fi
+
+if [ -e /mnt-root/@home_old ]; then
+    delete_subvolume_recursively /mnt-root/@home_old
 fi
 
 umount /mnt-root
