024e028ddd93ca344e0f723743b43ad885855a3a
Benzenec6h6 <aconitinec34h47no11@gmail.com>
2026-07-08 00:56:24 +0900

    Improvements in Evaluation Speed

--- modules/home/ollama.nix
+++ modules/home/ollama.nix
@@ -1,20 +1,28 @@
-{ pkgs, ... }:
 {
-    services.ollama = {
-        enable = true;
-        acceleration = "cuda";
-        
-        # 最新のエンジンを使う（unstableチャンネルを設定している前提）
-        package = pkgs.unstable.ollama;
+  pkgs,
+  inputs,
+  ...
+}: let
+  pkgs-unstable = import inputs.nixpkgs-unstable {
+    system = pkgs.stdenv.hostPlatform.system;
+    config.allowUnfree = true;
+  };
+in {
+  services.ollama = {
+    enable = true;
+    acceleration = "cuda";
 
-        # VRAM 6GBを効率よく使うためのチューニング
-        environmentVariables = {
-            OLLAMA_KEEP_ALIVE = "30m"; # 30分間はVRAMに保持（ロード待ち解消）
-            OLLAMA_FLASH_ATTENTION = "1"; # 推論の高速化とメモリ節約
-        };
-        #以下optionはdesktop購入後に検討
-        #host = "0.0.0.0";
-        #16 bit unsigned integer; between 0 and 65535 (both inclusive) default=11434
-        #port = 11434;
+    # 最新のエンジンを使う（unstableチャンネルを設定している前提）
+    package = pkgs-unstable.ollama;
+
+    # VRAM 6GBを効率よく使うためのチューニング
+    environmentVariables = {
+      OLLAMA_KEEP_ALIVE = "30m"; # 30分間はVRAMに保持（ロード待ち解消）
+      OLLAMA_FLASH_ATTENTION = "1"; # 推論の高速化とメモリ節約
     };
+    #以下optionはdesktop購入後に検討
+    #host = "0.0.0.0";
+    #16 bit unsigned integer; between 0 and 65535 (both inclusive) default=11434
+    #port = 11434;
+  };
 }
